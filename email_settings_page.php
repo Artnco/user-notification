bfea76fc7a65f902495210c80880812c957983e7
Artnco <145267846+Artnco@users.noreply.github.com>
2023-09-17 14:58:13 +0200

    Update email_settings_page.php

--- email_settings_page.php
+++ email_settings_page.php
@@ -1,38 +1,143 @@
 <?php
 
-function notification_email_settings_page_html() {
-    // Vérifiez les permissions de l'utilisateur
+function notification_settings_init() {
+    register_setting('notification_settings', 'notification_settings', array(
+        'default' => array(
+            'post_type' => '',
+            'meta_key' => '',
+        ),
+    ));
+
+}
+add_action('admin_init', 'notification_settings_init');
+
+
+function notification_settings_page_html() {
     if (!current_user_can('manage_options')) {
         return;
     }
 
-    // Récupérer les options existantes
-    $options = get_option('notification_email_settings');
+    if (isset($_GET['settings-updated'])) {
+        add_settings_error('notification_settings_messages', 'notification_settings_message', 'Paramètres enregistrés', 'updated');
+    }
 
-    // HTML pour la page de paramètres
-    echo '<div class="wrap notif_wrap">';
-    echo '<h1>Email Settings</h1>';
-    
-    echo '<form method="post" action="options.php" class="notif_form">';
-    
-    settings_fields('notification_email_settings');
-    do_settings_sections('notification_email_settings');
-    
-    // Champ pour le titre de l'email
-    echo '<div class="notif_form-group">';
-    echo '<label for="email_subject" class="notif_label">Email Subject</label>';
-    echo '<input type="text" id="email_subject" name="notification_email_settings[email_subject]" value="' . esc_attr($options['email_subject']) . '" class="notif_input" />';
-    echo '</div>';
+    settings_errors('notification_settings_messages');
 
-    // Champ pour le contenu de l'email
-    echo '<div class="notif_form-group">';
-    echo '<label for="email_body" class="notif_label">Email Body</label>';
-    echo '<textarea id="email_body" name="notification_email_settings[email_body]" rows="10" cols="50" class="notif_textarea">' . esc_textarea($options['email_body']) . '</textarea>';
-    echo '</div>';
+   
+    $options = get_option('notification_settings');
     
-    submit_button('Save Settings');
+   
+    $post_types = get_post_types(['public' => true], 'objects');
     
-    echo '</form>';
-    echo '</div>';
+    wp_enqueue_script('notification-user-settings', plugin_dir_url(__FILE__) . 'js/settings.js', array('jquery'), '1.0.0', true);
     
+    
+    wp_localize_script('notification-user-settings', 'notification_user_ajax_object', array(
+        'ajax_url' => admin_url('admin-ajax.php'),
+        'saved_meta_key' => isset($options['meta_key']) ? $options['meta_key'] : ''
+    ));
+
+    
+    ?>
+    
+    
+    <div class="wrap">
+    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+    
+    <p class="starting-mobule">To get started, insert the shortcode <b>[notification_bell]</b> wherever you want.</p>
+    
+    
+    <form action="options.php" method="post" id="settings-form">
+        <?php
+        settings_fields('notification_settings');
+        do_settings_sections('notification_settings');
+        
+        echo '<h2 class="settings-header">Select the post type *</h2>';
+        echo '<p class="settings-description">The selected post should be the one that allows message exchange between users</p>';
+        echo '<select name="notification_settings[post_type]" required>';
+        foreach ($post_types as $post_type) {
+            $selected = selected($options['post_type'], $post_type->name, false);
+            echo "<option value='" . esc_attr($post_type->name) . "' $selected>" . esc_html($post_type->labels->singular_name) . "</option>";
+        }
+        echo '</select>';
+        ?>
+        
+        <div class="meta-select-container meta-select-has-search" id="meta-select-meta-key">
+            <h2 class="settings-header">Meta Key Search *</h2>
+            <p class="settings-description">The selected meta field should be the one that dynamically adds the user ID who is supposed to receive the notification</p>
+            <input class="meta-select-search" type="text" placeholder="Enter at least 3 characters" minlength="3" autocomplete="off" required />
+            <svg class="meta-select-icon" viewBox="0 0 16 16" width="13" height="13">
+              
+            </svg>
+            <ul class="meta-select-results"></ul>
+            <input type="hidden" name="notification_settings[meta_key]" id="selected_meta_key" value="<?php echo esc_attr($options['meta_key']); ?>" required />
+        </div>
+
+        
+        <h2 class="settings-header">Select the redirection page *</h2>
+        <p class="settings-description">This page will be the page where the user, who receives the notification, will be redirected to.<br>For example, your page that displays the received messages.</p>
+        <?php
+        
+        $all_pages = get_pages();
+        ?>
+       
+        <select name="notification_settings[redirection_page]" id="redirection_page" required>
+            <?php foreach ($all_pages as $page) : ?>
+                <option value="<?php echo get_page_link($page->ID); ?>" 
+                    <?php selected($options['redirection_page'], get_page_link($page->ID)); ?>>
+                    <?php echo $page->post_title; ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        
+        <h2 class="settings-header">Notification Icon and Counter Colors</h2>
+        <p class="settings-description">Choose the colors for the notification icon and counter.</p>
+    
+        <p>
+            <label for="notification_bell_color">Notification Bell Color: </label>
+            <input type="color" id="notification_bell_color" name="notification_settings[bell_color]" value="<?php echo esc_attr($options['bell_color']); ?>" />
+        </p>
+    
+        <p>
+            <label for="notification_counter_bg_color">Notification Counter Background Color: </label>
+            <input type="color" id="notification_counter_bg_color" name="notification_settings[counter_bg_color]" value="<?php echo esc_attr($options['counter_bg_color']); ?>" />
+        </p>
+    
+        <p>
+            <label for="notification_counter_text_color">Notification Counter Text Color: </label>
+            <input type="color" id="notification_counter_text_color" name="notification_settings[counter_text_color]" value="<?php echo esc_attr($options['counter_text_color']); ?>" />
+        </p>
+    
+        <?php submit_button('Save Settings'); ?>
+    </form>
+    <p>To learn more about how the plugin works, you can <a href="https://github.com/Artnco/user-notification/tree/main" target="_blank">click here</a>.</p>
+</div>
+
+
+<?php
 }
+
+
+function notification_meta_key_search_callback() {
+    global $wpdb;
+
+    $search_str = isset($_POST['search_str']) ? sanitize_text_field($_POST['search_str']) : '';
+    if (strlen($search_str) < 3) {
+        wp_send_json_error('Please enter at least 3 characters.');
+    }
+
+    $results = $wpdb->get_results($wpdb->prepare(
+        "SELECT DISTINCT meta_key 
+         FROM $wpdb->postmeta 
+         WHERE meta_key LIKE %s", 
+        '%' . $wpdb->esc_like($search_str) . '%'
+    ));
+
+    if (!empty($results)) {
+        wp_send_json_success($results);
+    } else {
+        wp_send_json_error('No results found.');
+    }
+}
+
+add_action('wp_ajax_notification_meta_key_search', 'notification_meta_key_search_callback');
